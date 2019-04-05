eecc6d745826806fbe3013f98da864db614fa3b7
jimmytrevor <jimmytrevorsseg077@gmail.com>
2019-04-05 12:45:14 +0300

    Must Notice Board App Updates

--- app/src/main/java/com/example/mustnoticeboard/HomeActivity.java
+++ app/src/main/java/com/example/mustnoticeboard/HomeActivity.java
@@ -1,9 +1,15 @@
 package com.example.mustnoticeboard;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -11,18 +17,63 @@ public class HomeActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     //Declaring the Action bar Drawer Toogle
     private ActionBarDrawerToggle mToggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        navigationView=findViewById(R.id.drawable_nav);
 
         drawerLayout=findViewById(R.id.drawer);
         mToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);//The on and Off Definitions
         drawerLayout.addDrawerListener(mToggle);//Adding A listener to the Drawer Layout
         //Syncing the Toggle Button
         mToggle.syncState();
+
        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+       //setting default fragment on Activity creation
+        setFragemt(new HomeFragment());
+        //setting an onclick listener on the navigation view
+       navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+               int click=menuItem.getItemId();
+               switch (click){
+                   case R.id.home:
+                       setFragemt(new HomeFragment());
+                       return true;
+                   case R.id.account:
+                       return true;
+                   case R.id.setting:
+                       return true;
+                   case R.id.chat:
+                       return true;
+                   case R.id.help:
+                       setFragemt(new HelpFragment());
+                       return true;
+                   case R.id.share:
+                       Intent intent=new Intent(Intent.ACTION_SEND);
+                       intent.setType("text/plain");
+                       intent.putExtra("MY APP",true);
+                       startActivity(Intent.createChooser(intent,"SHARE THE APP NOW"));
+                       Toast.makeText(getApplicationContext(),"share",Toast.LENGTH_SHORT).show();
+                       return true;
+                   case R.id.about:
+                       setFragemt(new AboutFragment());
+                       return true;
+                   case R.id.privacy:
+                       return true;
+                       default:
+                           return false;
+               }
+           }
+       });
+    }
+
+    public void setFragemt(Fragment fragemt){
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,fragemt).commit();
 
     }
 }
